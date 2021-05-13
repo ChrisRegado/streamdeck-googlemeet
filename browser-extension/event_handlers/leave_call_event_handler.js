3b07c26798bc1e61f37b313d0e61cc247102224d
Andrew Chen <achen.code@gmail.com>
2021-05-13 13:15:03 -0700

    Attempt to go back to the home screen after hanging up.

--- browser-extension/event_handlers/leave_call_event_handler.js
+++ browser-extension/event_handlers/leave_call_event_handler.js
@@ -6,12 +6,20 @@ class LeaveCallEventHandler extends SDEventHandler {
     }
   }
 
-  _leaveCall = () => {
+  _leaveCall = async () => {
     const button = document.querySelector('[jsname="CQylAd"]');
     if (!button) {
       throw new ControlsNotFoundError("No Leave Call button found!")
     }
     button.click();
+
+    await new Promise(r => setTimeout(r, 3000));
+
+    const homeButton = document.querySelector('[jsname="dqt8Pb"]');
+    if (homeButton) {
+      // non-fatal error if we can't get back to the main Meet page
+      homeButton.click();
+    }
   }
 
 }
