9ffe487b0f2b647bef058181001fd0a7354a0799
Sam Galbraith <5651260+samjgalbraith@users.noreply.github.com>
2023-03-17 23:45:38 +1300

    Should basically work except for the icon I think

--- streamdeck-plugin/src/event_handlers/emoji_react_event_handler.py
+++ streamdeck-plugin/src/event_handlers/emoji_react_event_handler.py
@@ -1,25 +1,27 @@
 from event_handlers.base_event_handler import EventHandler
 
 EMOJI_REACTION_PARAMS = [
+#    EMOJI  ACTION_URI
     ("💖", "com.chrisregado.googlemeet.emojireact.sparklingheart"),
-    # "👍",
-    # "🎉",
-    # "👏",
-    # "😂",
-    # "😮",
-    # "😢",
-    # "🤔",
-    # "👎",
+    ("👍", "com.chrisregado.googlemeet.emojireact.thumbsup"),
+    ("🎉", "com.chrisregado.googlemeet.emojireact.partypopper"),
+    ("👏", "com.chrisregado.googlemeet.emojireact.clappinghands"),
+    ("😂", "com.chrisregado.googlemeet.emojireact.facewithtearsofjoy"),
+    ("😮", "com.chrisregado.googlemeet.emojireact.facewithtearsofjoy"),
+    ("😢", "com.chrisregado.googlemeet.emojireact.facewithopenmouth"),
+    ("🤔", "com.chrisregado.googlemeet.emojireact.thinkingface"),
+    ("👎", "com.chrisregado.googlemeet.emojireact.thumbsdown"),
 ]
 
-class EmojiReactEventHandler(EventHandler):
+class EmojiReactionEventHandler(EventHandler):
     """
     A reusable handler for Emoji reactions.
     """
 
-    def __init__(self, emoji_char, action_uri):
+    def __init__(self, stream_deck: "StreamDeckWebsocketClient", browser_manager: "BrowserWebsocketServer", emoji_char: str, action_uri: str):
         self.STREAM_DECK_ACTION = action_uri
         self._emoji_char = emoji_char
+        super().__init__(stream_deck, browser_manager)
 
     async def _key_up_handler(self, event: dict) -> None:
         message = self._make_simple_sd_event(f"emojiReact={self._emoji_char}")
