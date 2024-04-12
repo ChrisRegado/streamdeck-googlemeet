7afc77f06503b382a4fee2c82566a620300143d8
Alain <alain.devcs@gmail.com>
2024-04-12 08:55:02 +0200

    Update hand_event_handler for Italian language

--- browser-extension/event_handlers/hand_event_handler.js
+++ browser-extension/event_handlers/hand_event_handler.js
@@ -5,6 +5,7 @@ class HandEventHandler extends LabelBasedToggleEventHandler {
     ["Melden", "Meldung zurückziehen"],      // German
     ["Levantar la mano", "Bajar la mano"],   // Spanish
     ["Levantar a mão", "Abaixar a mão"],     // Portuguese
+    ["Alza la mano", "Abbassa la mano"],     // Italian
   ];
 
   _sendMuteState = () => {
