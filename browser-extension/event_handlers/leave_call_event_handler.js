0be4695ba1dd836bfc94b7853628cb0ef2c73149
Mike Garrett <mike.garrett@motorefi.com>
2024-08-08 17:42:50 -0400

    feat: Add ability to close leave dialog

--- browser-extension/event_handlers/leave_call_event_handler.js
+++ browser-extension/event_handlers/leave_call_event_handler.js
@@ -7,9 +7,18 @@ class LeaveCallEventHandler extends SDEventHandler {
   }
 
   _leaveCall = () => {
-    const button = document.querySelector('[jsname="CQylAd"]');
+    const leaveDialogPresent = document.querySelector('[jscontroller="ZakeSe"]');
+    if(leaveDialogPresent) {
+      this._leaveCallAction('[data-mdc-dialog-action="Pd96ce"]', 'Leave Call dialog');
+    } else {
+      this._leaveCallAction('[jsname="CQylAd"]', 'Leave Call button');
+    }
+  }
+
+  _leaveCallAction = (selector, buttonType) => {
+    const button = document.querySelector(selector);
     if (!button) {
-      throw new ControlsNotFoundError("No Leave Call button found!")
+      throw new ControlsNotFoundError("No " + buttonType + " found!")
     }
     button.click();
   }
