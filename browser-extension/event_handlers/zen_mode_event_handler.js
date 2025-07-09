a7a94c825f5a1f39a8c93d827a4cc85c5b4a3a2a
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2025-07-09 05:25:46 +0200

    feat: add zen mode (full screen) (#52)

---
+++ browser-extension/event_handlers/zen_mode_event_handler.js
@@ -0,0 +1,16 @@
+class ZenModeEventHandler extends ToggleEventHandler {
+
+  handleStreamDeckEvent = (message) => {
+    if (message.event === "toggleZenMode") {
+      this._toggleZenMode();
+    }
+  };
+
+  _toggleZenMode = () => {
+    // Credit for this code: https://github.com/verlok/google-meet-true-full-screen
+    const jsCtrlId = "hVZhab";
+    for (const controller of document.querySelectorAll(`[jscontroller="${jsCtrlId}"]`))
+      if (controller.style.display === "") controller.style.display = "none";
+      else controller.style.display = "";
+  };
+}
