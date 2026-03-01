3dfec9c3bb7a140f202ef28bdd0ccbeced896b75
Chris Regado <chris.regado@gmail.com>
2026-02-28 16:19:36 -0800

    Update Participants button for new Meet UI changes

--- browser-extension/event_handlers/participants_event_handler.js
+++ browser-extension/event_handlers/participants_event_handler.js
@@ -1,9 +1,18 @@
-class ParticipantsEventHandler extends SidepanelEventHandler {
+class ParticipantsEventHandler extends SDEventHandler {
 
   handleStreamDeckEvent = (message) => {
     if (message.event === "toggleParticipants") {
-      this._toggleSidepanel(1)
+      this._toggleParticipants();
     }
   }
+
+  _toggleParticipants = () => {
+      const participantsButton = document.querySelector('div[jsname="QbKf1d"]');
+      if (participantsButton) {
+        participantsButton.click();
+      } else {
+        throw new ControlsNotFoundError("No Participants button found!");
+      }
+  };
 
 }
